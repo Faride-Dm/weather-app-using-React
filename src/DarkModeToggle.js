921d60b75655549a9c53e0c059473a1325cbcece
Faride Danesh <37258724+Faride-Dm@users.noreply.github.com>
2023-04-25 10:57:23 -0400

    Added the DarkModeToggle component

---
+++ src/DarkModeToggle.js
@@ -0,0 +1,15 @@
+import React from "./react";
+
+function DarkModeToggle() {
+  <div className="d-flex darkModeToggle">
+    <img className="logo" src="/images/logo-shecodes.png" alt="logo" />
+    <input type="checkbox" className="checkbox" />
+    <label htmlFor="checkbox" className="label">
+      <i className="fas fa-moon"></i>
+      <i className="fas fa-sun"></i>
+      <div className="ball"></div>
+    </label>
+  </div>;
+}
+
+export default DarkModeToggle;
