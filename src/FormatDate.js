a9044863fd9e6cfffacc86b4201a2396f1fc2e21
Faride Danesh <37258724+Faride-Dm@users.noreply.github.com>
2023-04-26 17:35:32 -0400

    Added Bootstrap via npm

---
+++ src/FormatDate.js
@@ -0,0 +1,80 @@
+import React from "react";
+
+function formatDate() {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let day = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tueday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let weekDay = days[day];
+  dayOfTheWeek.innerHTML = `${weekDay} ${hours}:${minutes} <br />`;
+
+  let month = date.getMonth();
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  let yearMonths = months[month];
+
+  let dateInMonth = date.getDate();
+  let year = date.getFullYear();
+
+  if (yearMonths == "April" || yearMonths == "May" || yearMonths == "June") {
+    document.getElementById("backGround").style.backgroundImage =
+      "url(https://s3.amazonaws.com/shecodesio-production/uploads/files/000/069/047/original/spring.jpg?1677327335)";
+  } else if (
+    yearMonths == "July" ||
+    yearMonths == "August" ||
+    yearMonths == "September"
+  ) {
+    document.getElementById("backGround").style.backgroundImage =
+      "url(https://s3.amazonaws.com/shecodesio-production/uploads/files/000/069/045/original/summer.jpg?1677327246)";
+  } else if (
+    yearMonths == "October" ||
+    yearMonths == "November" ||
+    yearMonths == "December"
+  ) {
+    document.getElementById("backGround").style.backgroundImage =
+      "url(https://s3.amazonaws.com/shecodesio-production/uploads/files/000/069/046/original/autumn.jpg?1677327273)";
+  } else if (
+    (yearMonths == "December" && dateInMonth > 23) ||
+    (yearMonths == "January" && dateInMonth < 7)
+  ) {
+    document.getElementById("backGround").style.backgroundImage =
+      "url(https://s3.amazonaws.com/shecodesio-production/uploads/files/000/069/054/original/Chrismas.jpg?1677330137)";
+  } else {
+    document.getElementById("backGround").style.backgroundImage =
+      "url(https://s3.amazonaws.com/shecodesio-production/uploads/files/000/067/790/original/winter.jpg?1676478322)";
+  }
+
+  return `${weekDay} ${hours}:${minutes} <br /> ${dateInMonth} ${yearMonths} ${year} <br />`;
+}
+
+export default formatDate;
