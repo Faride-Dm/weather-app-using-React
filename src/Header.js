418d29b0fe5bde6ba465585eb737455b6714b06f
Faride Danesh <37258724+Faride-Dm@users.noreply.github.com>
2023-05-06 07:26:04 -0400

    Adjusted the background height

--- src/Header.js
+++ src/Header.js
@@ -2,15 +2,27 @@ import React from "react";
 import "./Header.css";
 
 function Header() {
-  <div className="d-flex Header">
-    <img className="logo" src="/images/logo-shecodes.png" alt="logo" />
-    <input type="checkbox" className="checkbox darkModeToggle" />
-    <label htmlFor="checkbox" className="label">
-      <i className="fas fa-moon"></i>
-      <i className="fas fa-sun"></i>
-      <div className="ball"></div>
-    </label>
-  </div>;
+  const checkbox = document.getElementById("checkbox");
+
+  checkbox.addEventListener("change", () => {
+    document.body.classList.toggle("dark");
+  });
+
+  return (
+    <div className="d-flex Header">
+      <img className="logo" src="/images/logo-shecodes.png" alt="logo" />
+      <input
+        type="checkbox"
+        className="checkbox darkModeToggle"
+        id="checkbox"
+      />
+      <label htmlFor="checkbox" className="label">
+        <i className="fas fa-moon"></i>
+        <i className="fas fa-sun"></i>
+        <div className="ball"></div>
+      </label>
+    </div>
+  );
 }
 
 export default Header;
