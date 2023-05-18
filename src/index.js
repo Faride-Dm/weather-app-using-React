5c76e5afb5997388b2f9604161304c8296a7d965
Faride Danesh <37258724+Faride-Dm@users.noreply.github.com>
2023-05-18 19:02:15 -0400

    Added the theme to index.js to use

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
+import ThemeContextWrapper from "./ThemeContextWrapper";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ThemeContextWrapper>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ThemeContextWrapper>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
